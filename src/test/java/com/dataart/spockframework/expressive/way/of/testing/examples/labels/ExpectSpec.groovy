652ca24e3edd7a3ceb280670bcb77aa84facc526
Mateusz Mulawa <mateusz.mulawa@gmail.com>
2018-06-18 21:52:15 +0200

    Groovy truth example

--- src/test/java/com/dataart/spockframework/expressive/way/of/testing/examples/labels/ExpectSpec.groovy
+++ src/test/java/com/dataart/spockframework/expressive/way/of/testing/examples/labels/ExpectSpec.groovy
@@ -10,15 +10,21 @@ class ExpectSpec extends Specification {
         and: 'range from 1 to 5 is of size 5'
             (1..5).size() == 5
         and: 'closure returns last statement'
-            def c = {
+            def closure = {
                 1
                 'last'
             }
-            c() == 'last'
+            closure() == 'last'
         and: 'to make assertion in closure body you have to use assert'
             (1..5).each { it ->
                 assert it > 0
                 assert it < 6
             }
+        and: 'any value can evaluate to Groovy truth'
+            !''
+            'name'
+            def anotherClosure = { '' }
+            !anotherClosure()
+
     }
 }
